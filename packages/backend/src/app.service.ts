ba0f28422bd94e8f5743c5c4ff4e80d261a42af8
Themion <themion@naver.com>
2023-04-15 00:36:57 +0900

    feat : common as cjs/esm
    DESC
    ----
    @my-task/common 모듈을 esm과 cjs 두 방식으로 모두 사용할 수 있도록 수정

--- packages/backend/src/app.service.ts
+++ packages/backend/src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { hello } from '@my-task/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return hello();
   }
 }
