d19537f70b63d88fc5b121be76396da9a98af7ec
Themion <themion@naver.com>
2023-04-16 19:04:56 +0900

    feat : frontend에 test 추가
    DESC
    ----
    vitest와 testing-library를 이용한 frontend 테스트 환경 구축

---
+++ packages/frontend/src/App.test.tsx
@@ -0,0 +1,11 @@
+import App from "./App";
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+
+describe("App", () => {
+  it("renders", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Vite \+ React/i);
+    expect(linkElement).toBeDefined();
+  });
+});
