f108193024e22e65ddecb60ac7ac9c5d2802f7d1
Themion <themion@naver.com>
2023-04-22 15:40:37 +0900

    feat : docker를 이용한 배포
    DESC
    ----
    docker를 이용해 monorepo 앱을 간단하게 배포할 수 있도록 설정
    
    NOTE
    ----
    env 혹은 healthcheck 등은 고려하지 않음

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+  },
   resolve: {
     alias: [{ find: '~', replacement: resolve(__dirname, 'src') }],
   },
