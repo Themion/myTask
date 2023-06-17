3262c8102cda7c4a27e92b9c127e7a9e86b03329
Themion <themion@naver.com>
2023-06-17 20:37:21 +0900

    chore : DB 이미지를 Action에 추가
    DESC
    ----
    - Backend 테스트를 위해 DB Image를 GitHub Action에서 build 및 start
    - DB Image 종료
    - DB Image가 종료될 시 자동으로 삭제됨
    
    NOTE
    ----
    - GitHub Secret을 이용해 환경 변수를 설정해주어야 함

--- packages/database/start.sh
+++ packages/database/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source ".env"
-docker run --env-file ./.env -p $DB_PORT:5432 -d database
+source "../../.env"
+docker run --env-file ../../.env --name database --rm -p $DB_PORT:5432 -d database
