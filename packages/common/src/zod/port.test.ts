85b732e1c148e114bd8a9a97225d39c87394b1fd
Themion <themion@naver.com>
2023-06-24 14:45:26 +0900

    feat : DATABASE에 사용할 환경 변수 검증 로직 추가
    DESC
    ----
    - PORT는 범위 [1025, 65525]에 존재하는 정수여야 한다
    - HOST는 0으로 시작하지 않는 IP 주소, 혹은 localhost여야 한다.
    
    NOTE
    ----
    - 테스트 코드를 describe를 이용해 같은 분류끼리 묶음
    
    Close \#51
    
    # ----------------
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    # 예) Close #7
    
    # ----------------
    # feat : 새로운 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refact : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    # ----------------
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch feature/db-connection-03-backend-env-test
    # Changes to be committed:
    #	modified:   packages/backend/src/env/validate.spec.ts
    #	modified:   packages/backend/src/env/validate.ts
    #	new file:   packages/common/src/zod/host.test.ts
    #	new file:   packages/common/src/zod/host.ts
    #	modified:   packages/common/src/zod/index.ts
    #	modified:   packages/common/src/zod/numericString.test.ts
    #	modified:   packages/common/src/zod/port.test.ts
    #	modified:   packages/common/src/zod/port.ts
    #
    # Changes not staged for commit:
    #	modified:   packages/backend/jest.config.json
    #

--- packages/common/src/zod/port.test.ts
+++ packages/common/src/zod/port.test.ts
@@ -3,58 +3,76 @@ import { describe, it } from 'mocha';
 import PORT_RULE from './port';
 
 describe('port', () => {
-  it('should work with numeric number in range', () => {
-    const before = '3000';
-    const after = PORT_RULE.parse(before);
-    expect(after).to.equal(parseInt(before));
-  });
+  const SYSTEM_PORT_MAX = 1024;
+  const PORT_MAX = 65535;
 
-  describe('should make error with range', () => {
-    it('value < 0 (out of port range)', () => {
-      const before = '-123';
-      expect(() => PORT_RULE.parse(before)).to.throw();
-    });
+  const randint = (min: number, max: number) => {
+    min = Math.floor(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max + 1 - min)) + min;
+  };
 
-    it('0 <= vaule <= 1024 (system port range)', () => {
-      const before = '123';
-      expect(() => PORT_RULE.parse(before)).to.throw();
-    });
-
-    it('65535 < vaule (out of port range)', () => {
-      const before = '70000';
-      expect(() => PORT_RULE.parse(before)).to.throw();
+  describe('should work with', () => {
+    it('1024 < value <= 65535 (numeric number in range)', () => {
+      const after = randint(SYSTEM_PORT_MAX, PORT_MAX);
+      const before = after.toString();
+      expect(after).equal(parseInt(before));
     });
   });
 
-  describe('should make error with type', () => {
-    it('non-numeric string', () => {
-      const before = 'nom-numeric';
-      expect(() => PORT_RULE.parse(before)).to.throw();
+  describe('should throw error when', () => {
+    describe('value', () => {
+      it('value != round(value)', () => {
+        const before = randint(SYSTEM_PORT_MAX, PORT_MAX) + Math.random();
+        expect(() => PORT_RULE.parse(before)).throw();
+      });
+
+      it('value < 0 (out of port range)', () => {
+        const before = (randint(0, PORT_MAX) * -1).toString();
+        expect(() => PORT_RULE.parse(before)).throw();
+      });
+
+      it('0 <= vaule <= 1024 (system port range)', () => {
+        const before = randint(0, SYSTEM_PORT_MAX).toString();
+        expect(() => PORT_RULE.parse(before)).throw();
+      });
+
+      it('65535 < vaule (out of port range)', () => {
+        const before = randint(PORT_MAX, PORT_MAX + 1000).toString();
+        expect(() => PORT_RULE.parse(before)).throw();
+      });
     });
 
-    it('number', () => {
-      const before = 123;
-      expect(() => PORT_RULE.parse(before)).to.throw();
-    });
+    describe('type', () => {
+      it('non-numeric string', () => {
+        const before = 'nom-numeric';
+        expect(() => PORT_RULE.parse(before)).throw();
+      });
 
-    it('boolean', () => {
-      const before = true;
-      expect(() => PORT_RULE.parse(before)).to.throw();
-    });
+      it('number', () => {
+        const before = 123;
+        expect(() => PORT_RULE.parse(before)).throw();
+      });
 
-    it('undefined', () => {
-      const before = undefined;
-      expect(() => PORT_RULE.parse(before)).to.throw();
-    });
+      it('boolean', () => {
+        const before = true;
+        expect(() => PORT_RULE.parse(before)).throw();
+      });
 
-    it('null', () => {
-      const before = null;
-      expect(() => PORT_RULE.parse(before)).to.throw();
-    });
+      it('undefined', () => {
+        const before = undefined;
+        expect(() => PORT_RULE.parse(before)).throw();
+      });
 
-    it('function', () => {
-      const before = () => {};
-      expect(() => PORT_RULE.parse(before)).to.throw();
+      it('null', () => {
+        const before = null;
+        expect(() => PORT_RULE.parse(before)).throw();
+      });
+
+      it('function', () => {
+        const before = () => {};
+        expect(() => PORT_RULE.parse(before)).throw();
+      });
     });
   });
 });
