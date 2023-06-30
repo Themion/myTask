2232dc892004cd4bcf55ac3ca4d3f3104ffa63cd
Themion <themion@naver.com>
2023-06-30 20:31:48 +0900

    feat : CORS 설정
    DESC
    ----
    - Frontend에서 요청을 보낼 수 있도록 CORS 설정

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -2,16 +2,25 @@ import { INestApplication } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '~/app.module';
-import { Env } from './types';
+import { Env } from '~/types';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(getPortFromConfig(app));
+  const { HOST, BE_PORT, FE_PORT } = getConfig(app);
+
+  const corsOptions = { origin: `http://${HOST}:${FE_PORT}` };
+  app.enableCors(corsOptions);
+
+  await app.listen(BE_PORT);
 }
 
-function getPortFromConfig(app: INestApplication) {
-  const config = app.get(ConfigService<Env>);
-  return config.getOrThrow<Env['BE_PORT']>('BE_PORT');
+function getConfig(app: INestApplication) {
+  const configService = app.get(ConfigService<Env>);
+  const HOST = configService.getOrThrow<Env['HOST']>('HOST');
+  const BE_PORT = configService.getOrThrow<Env['BE_PORT']>('BE_PORT');
+  const FE_PORT = configService.getOrThrow<Env['FE_PORT']>('FE_PORT');
+
+  return { HOST, BE_PORT, FE_PORT };
 }
 
 bootstrap();
