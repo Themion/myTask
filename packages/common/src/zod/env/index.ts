020cd4ffe122dbd1966a56196d1c9a4edce43ef8
Themion <themion@naver.com>
2023-07-02 16:36:47 +0900

    refact : 환경 변수 검증에 사용되던 zod 로직을 하위 폴더로 이동
    DESC
    ----
    - zod 디렉토리에 DTO 검증을 위한 코드를 추가할 예정이므로 디렉토리 구조를 개선

---
+++ packages/common/src/zod/env/index.ts
@@ -0,0 +1,4 @@
+import HOST_RULE from './host';
+import NUMERIC_STRING_RULE from './numericString';
+import PORT_RULE from './port';
+export { HOST_RULE, NUMERIC_STRING_RULE, PORT_RULE };
--- packages/common/src/zod/index.ts
+++ packages/common/src/zod/index.ts
@@ -1,4 +1,1 @@
-import HOST_RULE from './host';
-import NUMERIC_STRING_RULE from './numericString';
-import PORT_RULE from './port';
-export { HOST_RULE, NUMERIC_STRING_RULE, PORT_RULE };
+export * from './env';
