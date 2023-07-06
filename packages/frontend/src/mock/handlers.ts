f2759030e5ce52bff709f1e127cbcae51c88668a
Themion <themion@naver.com>
2023-07-06 22:12:55 +0900

    fix : 서버에서 throw되는 경우를 mock에 추가
    DESC
    ----
    - 서버에서 `HttpException`을 throw한 경우를 mock하는 경우를 추가

--- packages/frontend/src/mock/handlers.ts
+++ packages/frontend/src/mock/handlers.ts
@@ -7,4 +7,7 @@ export const handlers = [
     const body = await req.json();
     return res(ctx.status(200), ctx.json(body));
   }),
+  rest.get(`${BE_ORIGIN}/error`, (_, res, ctx) =>
+    res(ctx.status(400), ctx.json({ errorMessage: 'Error thrown for unknown reason.' })),
+  ),
 ];
--- packages/frontend/src/mock/mock.test.ts
+++ packages/frontend/src/mock/mock.test.ts
@@ -3,17 +3,26 @@ import { BE_ORIGIN } from '~/constants';
 import { server } from '~/mock';
 
 describe('mock', () => {
-  beforeAll(() => server.listen());
+  beforeAll(() => server.listen({ onUnhandledRequest: 'bypass' }));
   // https://stackoverflow.com/questions/76046546/fetch-error-typeerror-err-invalid-url-invalid-url-for-requests-made-in-test
   beforeEach(() => location.replace(BE_ORIGIN));
   afterEach(() => server.resetHandlers());
   afterAll(() => server.close());
 
-  it('shold work', async () => {
-    const path = '/';
-    const result = await fetch(`${BE_ORIGIN}${path}`);
-    const data = await result.json();
-    expect(data).toStrictEqual({ foo: 'bar' });
+  describe('should work when', () => {
+    it('returned from server', async () => {
+      const path = '/';
+      const result = await fetch(`${BE_ORIGIN}${path}`);
+      const data = await result.json();
+      expect(data).toStrictEqual({ foo: 'bar' });
+    });
+
+    it('error thrown in server', async () => {
+      const path = '/error';
+      const result = await fetch(`${BE_ORIGIN}${path}`);
+      expect(result.ok).toEqual(false);
+      expect((await result.json()).errorMessage).toBeDefined();
+    });
   });
 
   describe('should throw error when', () => {
