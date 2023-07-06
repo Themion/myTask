7dab11fa241980b89dcc64c1d1e587423ca5bd84
Themion <themion@naver.com>
2023-07-06 22:19:02 +0900

    fix : 서버와 연결이 필요한 테스트에서 mock path 인증
    DESC
    ----
    - 서버와의 통신을 mocking하는 테스트에서 존재하지 않는 path로 요청을 보낼 경우 오류를 반환

--- packages/frontend/src/App.test.tsx
+++ packages/frontend/src/App.test.tsx
@@ -2,8 +2,16 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import App from '~/App';
+import { BE_ORIGIN } from '~/constants';
+import { server } from '~/mock';
 
 describe('App', () => {
+  beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+  // https://stackoverflow.com/questions/76046546/fetch-error-typeerror-err-invalid-url-invalid-url-for-requests-made-in-test
+  beforeEach(() => location.replace(BE_ORIGIN));
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
+
   it('renders', () => {
     render(
       <QueryClientProvider client={new QueryClient()}>
