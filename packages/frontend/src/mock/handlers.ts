1b45b7e14e9d75842aaaa8bdc5d85d63f65f7f5d
Themion <themion@naver.com>
2023-07-06 23:05:12 +0900

    feat : 회원가입 API 연결
    DESC
    ----
    - 백엔드의 회원가입 API에 대응되는 프론트엔드 API 생성

--- packages/frontend/src/mock/handlers.ts
+++ packages/frontend/src/mock/handlers.ts
@@ -1,13 +1,30 @@
+import { createUserDTO } from '@my-task/common';
 import { rest } from 'msw';
 import { BE_ORIGIN } from '~/constants';
 
+const mockDir = (path: string) => `${BE_ORIGIN}${path}`;
+
 export const handlers = [
-  rest.get(`${BE_ORIGIN}/`, (_, res, ctx) => res(ctx.status(200), ctx.json({ foo: 'bar' }))),
-  rest.post(`${BE_ORIGIN}/`, async (req, res, ctx) => {
+  rest.get(mockDir(''), (_, res, ctx) => res(ctx.status(200), ctx.json({ foo: 'bar' }))),
+  rest.post(mockDir(''), async (req, res, ctx) => {
     const body = await req.json();
     return res(ctx.status(200), ctx.json(body));
   }),
   rest.get(`${BE_ORIGIN}/error`, (_, res, ctx) =>
     res(ctx.status(400), ctx.json({ errorMessage: 'Error thrown for unknown reason.' })),
   ),
+
+  rest.post(mockDir('/auth'), async (req, res, ctx) => {
+    const body = await req.json();
+
+    ctx.delay();
+
+    try {
+      const dto = createUserDTO.parse(body);
+      const id = Math.floor(Math.random() * 10);
+      return res(ctx.status(200), ctx.json({ ...dto, id }));
+    } catch (error) {
+      return res(ctx.status(400), ctx.json({ errorMessage: 'Wrong DTO: try again!' }));
+    }
+  }),
 ];
