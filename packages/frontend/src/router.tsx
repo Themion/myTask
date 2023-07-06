75f3b448efb6860172cd1ed62c9eefe43b3d33c5
Themion <themion@naver.com>
2023-07-06 23:01:29 +0900

    feat : 회원가입 페이지 생성
    DESC
    ----
    - email을 입력해 회원가입을 진행할 수 있는 Join 페이지를 생성
    
    NOTE
    ----
    - 실제로 사용하는 이메일인지 확인하는 로직은 구현되어 있지 않음

--- packages/frontend/src/router.tsx
+++ packages/frontend/src/router.tsx
@@ -1,11 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { Root } from '~/routes';
+import Join from '~/routes/Join';
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-  },
+  { path: '/', element: <Root /> },
+  { path: '/join', element: <Join /> },
 ]);
 
 export default router;
