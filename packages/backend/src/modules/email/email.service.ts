516f61df7c173b48f15d99f1615b013fdd207532
Themion <themion@naver.com>
2023-07-08 22:58:22 +0900

    feat : E-Mail 전송 기능 생성
    DESC
    ----
    - EmailService에 E-Mail 전송 기능 생성
    
    TEST
    ---
    1. 환경 변수 `EMAIL_TEST_ADDRESS`에 E-Mail 값을 저장
    2. 테스트 코드를 실행한 뒤 환경 변수로 지정한 이메일에 테스트 이메일이 왔는지 확인
      - mailtrap 등 테스트 환경을 사용하고 있다면 해당 환경 역시 확인
    
    NOTE
    ----
    - E-Mail 기능은 controller를 통해 접근하는 기능이 아님

--- packages/backend/src/modules/email/email.service.ts
+++ packages/backend/src/modules/email/email.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { createTransport } from 'nodemailer';
+import { z } from 'zod';
 import { Env } from '~/types';
 
 @Injectable()
 export class EmailService {
+  private readonly transport;
+  private readonly sender;
+
   constructor(configService: ConfigService<Env>) {
     // https://velog.io/@mimi0905/Nodemailer%EB%A1%9C-%EB%A9%94%EC%9D%BC-%EB%B3%B4%EB%82%B4%EA%B8%B0-with-%EC%B2%A8%EB%B6%80%ED%8C%8C%EC%9D%BC
-    createTransport({
+    this.transport = createTransport({
       host: configService.getOrThrow<Env['EMAIL_HREF']>('EMAIL_HREF'),
       port: configService.getOrThrow<Env['EMAIL_PORT']>('EMAIL_PORT'),
       secure: false,
@@ -15,6 +19,17 @@ export class EmailService {
         user: configService.getOrThrow<Env['EMAIL_USER']>('EMAIL_USER'),
         pass: configService.getOrThrow<Env['EMAIL_PASS']>('EMAIL_PASS'),
       },
+    });
+
+    this.sender = configService.getOrThrow<Env['EMAIL_ADDRESS']>('EMAIL_ADDRESS');
+  }
+
+  sendTestEmail(target: string) {
+    return this.transport.sendMail({
+      from: this.sender,
+      to: z.string().email().parse(target),
+      subject: 'test',
+      html: 'test mail',
     });
   }
 }
