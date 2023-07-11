2b438f2e41c1f6201657a85b80243494e143baf8
Themion <themion@naver.com>
2023-07-11 22:12:59 +0900

    feat : welcome 페이지에 잘못된 값이 들어올 경우 에러 페이지 표시
    DESC
    ----
    - Welcome 페이지의 uuid 위치에 아래 값이 들어올 경우 에러 페이지 표시
      - uuid가 들어오지 않은 경우
      - uuid가 아닌 문자열이 들어올 경우
      - uuid에 대응되는 E-Mail이 이미 처리된 경우
        - 이 경우는 uuid가 들어오지 않은 경우와 같이 처리됨 - 에러 처리 필요
    
    TEST
    ----
    1. 백엔드 서버와 프론트엔드 서버를 각각 실행
    2. 아래 경우에 해당되는 문자열을 프론트엔드의 /welcome/${value}에 넣을 경우 에러 페이지가 표시되는지 확인
      - 빈 문자열
      - uuid가 아닌 문자열이 들어올 경우
      - E-Mail을 통해 받은 적 없는 uuid
      - 이미 한 번 사용한 uuid

--- packages/frontend/src/routes/Welcome/index.tsx
+++ packages/frontend/src/routes/Welcome/index.tsx
@@ -1,9 +1,15 @@
 import { RouteObject } from 'react-router-dom';
+import ErrorPage from './error';
 import Welcome from './page';
 
 const welcomeRouteObject: RouteObject = {
   path: 'welcome',
+  errorElement: <ErrorPage />,
   children: [
+    {
+      path: '',
+      element: <ErrorPage />,
+    },
     {
       path: ':uuid',
       element: <Welcome />,
