4e8c581069f82768906c7405d6281bc259ce986c
Themion <themion@naver.com>
2023-07-11 22:04:25 +0900

    feat : 회원가입 후 표시할 Welcome 페이지 생성
    DESC
    ----
    - 회원가입 요청 시 생성된 uuid를 param을 통해 인자로 받을 경우 보여줄 welcome 페이지 생성
    
    TEST
    ----
    1. 백엔드 서버와 프론트엔드 서버를 각각 실행
    2. 프론트엔드 페이지의 /join 디렉토리에 접속한 뒤 E-Mail을 입력하고 회원가입 버튼을 클릭
    3. 입력한 주소로 전송된 이메일에 적힌 URL을 클릭
    4. Welcome 페이지가 제대로 표시되는지 확인
    
    NOTE
    ----
    - E-Mail을 받지 못하거나 잘못된 값이 들어올 경우는 추가 PR을 통해 push

--- packages/frontend/src/routes/index.ts
+++ packages/frontend/src/routes/index.ts
@@ -1,7 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
+import welcomeRouteObject from '~/routes/Welcome';
 import joinRouteObject from './Join';
 import rootRouteObject from './Root';
 
-const router = createBrowserRouter([rootRouteObject, joinRouteObject]);
+const router = createBrowserRouter([rootRouteObject, joinRouteObject, welcomeRouteObject]);
 
 export default router;
