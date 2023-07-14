65b3a75a1b0ac9bb3994c2962f95ef00ca9021a6
Themion <themion@naver.com>
2023-07-14 20:53:06 +0900

    feat : mockConfigModule의 env 파싱
    DESC
    ----
    - 기존 `mockConfigModule`에서 parse하지 않던 process.env를 파싱

--- packages/backend/src/mock/modules/config.module.ts
+++ packages/backend/src/mock/modules/config.module.ts
@@ -1,3 +1,4 @@
 import { ConfigModule } from '@nestjs/config';
-const mockConfigModule = async () => ConfigModule.forFeature(() => process.env);
+import { validate } from '~/env';
+const mockConfigModule = async () => ConfigModule.forFeature(() => validate(process.env));
 export default mockConfigModule;
