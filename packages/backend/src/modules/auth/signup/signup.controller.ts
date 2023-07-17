0ffbdeebcb5131a69cfe9640a2e498f3aa6228c3
Themion <themion@naver.com>
2023-07-17 13:52:29 +0900

    feat : auth 모듈의 하위 모듈 생성
    DESC
    ----
    - auth 모듈에서 회원가입 기능을 전담할 하위 모듈인 signup 모듈 생성
    
    NOTE
    ----
    - 테스트 코드 및 mock 모듈은 기존의 모듈을 이름을 바꿔서 사용할 예정

---
+++ packages/backend/src/modules/auth/signup/signup.controller.ts
@@ -0,0 +1,39 @@
+import { confirmJoinUserDTO, requestJoinUserDTO } from '@my-task/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { EmailService } from '~/modules/email/email.service';
+import { GroupService } from '~/modules/group/group.service';
+import { SignUpService } from './signup.service';
+
+@Controller('signup')
+export class SignUpController {
+  constructor(
+    private readonly signupService: SignUpService,
+    private readonly emailService: EmailService,
+    private readonly groupService: GroupService,
+  ) {}
+
+  @Post('syn')
+  requestSignUpUser(@Body() body: any) {
+    const result = requestJoinUserDTO.safeParse(body);
+    if (!result.success) throw new BadRequestException('Wrong DTO: try again!');
+    const { data } = result;
+
+    const uuid = this.signupService.requestSignUpUser(data);
+    // E-Mail 송신은 동기적으로 진행할 필요 없음
+    this.emailService.sendJoinEmail(data.email, uuid);
+
+    return data;
+  }
+
+  @Post('ack')
+  async confirmSignUpUser(@Body() body: any) {
+    const result = confirmJoinUserDTO.safeParse(body);
+    if (!result.success) throw new BadRequestException('Wrong DTO: try again!');
+    const { data } = result;
+
+    const newUser = await this.signupService.confirmSignUpUser(data);
+    await this.groupService.createGroup(newUser);
+
+    return newUser;
+  }
+}
