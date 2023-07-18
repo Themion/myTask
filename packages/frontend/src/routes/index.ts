c02b13fcb4429bdf2cbd379756d43d22000ae841
Themion <themion@naver.com>
2023-07-18 16:45:43 +0900

    feat : 로그인 요청 페이지 생성
    DESC
    ----
    - 로그인 요청 메일을 전송하는 페이지 생성
    
    NOTE
    ----
    - 가입하지 않은 사용자를 거부하는 기능은 구현되지 않음

--- packages/frontend/src/routes/index.ts
+++ packages/frontend/src/routes/index.ts
@@ -1,8 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
-import welcomeRouteObject from '~/routes/Welcome';
 import rootRouteObject from './Root';
+import signInRouteObject from './SignIn';
 import signUpRouteObject from './SignUp';
+import welcomeRouteObject from './Welcome';
 
-const router = createBrowserRouter([rootRouteObject, signUpRouteObject, welcomeRouteObject]);
+const router = createBrowserRouter([
+  rootRouteObject,
+  signInRouteObject,
+  signUpRouteObject,
+  welcomeRouteObject,
+]);
 
 export default router;
