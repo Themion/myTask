5aaf5b852dd5ddaba52278673e94d09a9218a528
Themion <themion@naver.com>
2023-07-24 00:20:50 +0900

    feat : 로그인 요청 기능 생성
    DESC
    ----
    - 로그인 요청 기능 및 API 생성
    
    NOTE
    ----
    - 회원가입하지 않은 이메일이 들어온 경우 reject하는 메소드 생성하지 않음

--- packages/backend/src/mock/modules/signin.module.ts
+++ packages/backend/src/mock/modules/signin.module.ts
@@ -1,24 +1,30 @@
 import { Provider } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
-import { MockSignInService } from '~/mock/services';
+import mockConfigModule from '~/mock/modules/config.module';
+import { MockEmailService, MockSignInService } from '~/mock/services';
 import { SignInController } from '~/modules/auth/signin/signin.controller';
 import { SignInService } from '~/modules/auth/signin/signin.service';
+import { EmailService } from '~/modules/email/email.service';
 
 type Props = {
   signInService?: MockSignInService;
+  emailService?: MockEmailService;
 };
 
-const mockSignInModule = async ({ signInService }: Props) => {
+const mockSignInModule = async ({ signInService, emailService }: Props) => {
   const providers: Provider[] = [SignInService];
+  if (emailService) providers.push(EmailService);
 
   const useController = !!signInService;
   const controllers = useController ? [SignInController] : [];
 
   const moduleFactory = Test.createTestingModule({
+    imports: [await mockConfigModule()],
     providers,
     controllers,
   });
 
+  if (emailService) moduleFactory.overrideProvider(EmailService).useValue(emailService);
   if (signInService) moduleFactory.overrideProvider(SignInService).useValue(signInService);
 
   return moduleFactory.compile();
--- packages/backend/src/modules/auth/signin/signin.service.ts
+++ packages/backend/src/modules/auth/signin/signin.service.ts
@@ -1,4 +1,20 @@
+import { RequestSignInDTO } from '@my-task/common';
 import { Injectable } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
-export class SignInService {}
+export class SignInService {
+  private readonly uuidToEmail = new Map<string, RequestSignInDTO>();
+  private readonly emailToUUID = new Map<string, string>();
+
+  async requestSignIn(dto: RequestSignInDTO) {
+    if (this.emailToUUID.has(dto.email)) return this.emailToUUID.get(dto.email) as string;
+
+    const uuid = uuidv4();
+
+    this.uuidToEmail.set(uuid, dto);
+    this.emailToUUID.set(dto.email, uuid);
+
+    return uuid;
+  }
+}
