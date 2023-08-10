36c8f95b6bbcd989e1d9bd3f6e579152c4816183
Themion <themion@naver.com>
2023-08-10 23:57:16 +0900

    feat : 중복된 auth 요청이 들어올 경우 기존 요청 만료
    DESC
    ----
    - 중복된 auth 요청이 들어올 경우 기존의 auth 요청은 만료시킴

--- packages/backend/src/constants/cache/tableName.ts
+++ packages/backend/src/constants/cache/tableName.ts
@@ -1,10 +1,9 @@
 const RT2Email = 'RT2Email';
 const pendingEmail = 'pendingEmail';
-const signUpTable = 'signUpTable';
-const signInTable = 'signInTable';
 const uuidToEmail = 'uuidToEmail';
+const emailToUUID = 'emailToUUID';
 
-const CACHE_TABLE_NAME = { pendingEmail, RT2Email, signUpTable, signInTable, uuidToEmail };
+const CACHE_TABLE_NAME = { pendingEmail, RT2Email, emailToUUID, uuidToEmail };
 Object.freeze(CACHE_TABLE_NAME);
 
 export default CACHE_TABLE_NAME;
