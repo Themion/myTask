ea7073bd8e9cad4b85d82dbe1fc6b6f4b1dbbd9c
Themion <themion@naver.com>
2023-08-10 11:01:05 +0900

    feat : frontend의 mock handler 수정
    DESC
    ----
    - mock handler를 여러 파일로 쪼갬

---
+++ packages/frontend/src/mock/handlers/index.ts
@@ -0,0 +1,16 @@
+import { rest } from 'msw';
+import authHandlers from './auth';
+import mockDir from './mockDir';
+
+export const handlers = [
+  rest.get(mockDir(''), (_, res, ctx) => res(ctx.status(200), ctx.json({ foo: 'bar' }))),
+  rest.post(mockDir(''), async (req, res, ctx) => {
+    const body = await req.json();
+    return res(ctx.status(200), ctx.json(body));
+  }),
+  rest.get(mockDir('/error'), (_, res, ctx) =>
+    res(ctx.status(400), ctx.json({ errorMessage: 'Error thrown for unknown reason.' })),
+  ),
+
+  ...authHandlers,
+];
---
+++ packages/frontend/src/mock/handlers/mockDir.ts
@@ -0,0 +1,3 @@
+import { BE_ORIGIN } from '~/constants';
+const mockDir = (path: string) => `${BE_ORIGIN}${path}`;
+export default mockDir;
