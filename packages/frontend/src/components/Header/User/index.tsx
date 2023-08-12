13eb3f4cd71868508b8e98fdeca2d951c885097a
Themion <themion@naver.com>
2023-08-12 20:50:15 +0900

    feat : dialog의 margin 조정
    DESC
    ----
    - 회원 메뉴 dropdown을 dialog에서 div로 변경

--- packages/frontend/src/components/Header/User/index.tsx
+++ packages/frontend/src/components/Header/User/index.tsx
@@ -4,38 +4,38 @@ import { shouldRefreshAtom } from '~/recoil/atoms';
 
 const HeaderUser = () => {
   const parentRef = useRef<HTMLDivElement>(null);
-  const dialogRef = useRef<HTMLDialogElement>(null);
+  const dropdownRef = useRef<HTMLDivElement>(null);
   const refreshed = useRecoilValue(shouldRefreshAtom);
 
   const getElement = () => {
     const wrapper = parentRef.current;
-    const dialog = dialogRef.current;
+    const dropdown = dropdownRef.current;
 
-    return { dialog, wrapper };
+    return { dropdown, wrapper };
   };
 
   const onMenuClick = () => {
-    const { dialog } = getElement();
-    if (!dialog) return;
+    const { dropdown } = getElement();
+    if (!dropdown) return;
 
-    dialog.open ? dialog.close() : dialog.show();
+    dropdown.classList.toggle('hidden');
   };
 
   globalThis.addEventListener('click', (e) => {
     e.preventDefault();
 
-    const { dialog, wrapper } = getElement();
-    if (!dialog || !wrapper) return;
+    const { dropdown, wrapper } = getElement();
+    if (!wrapper || !dropdown) return;
 
-    if (!wrapper.contains(e.target as Node)) dialog.close();
+    if (!wrapper.contains(e.target as Node)) dropdown.classList.add('hidden');
   });
 
   return (
     <div className="header__user" ref={parentRef}>
       <span onClick={onMenuClick}>user menu</span>
-      <dialog className="header__user--dialog" ref={dialogRef}>
+      <div className="header__user--dropdown hidden" ref={dropdownRef}>
         {refreshed ? 'sign out' : 'sign in'}
-      </dialog>
+      </div>
     </div>
   );
 };
