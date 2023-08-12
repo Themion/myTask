4d4c7cf9c5d2ee47275da31f6090bf0224a21b59
Themion <themion@naver.com>
2023-08-12 18:53:43 +0900

    feat : 사용자 메뉴 버튼 클릭 시 사용자 메뉴 dialog 표시
    DESC
    ----
    - 사용자 메뉴 버튼 클릭 시 사용자 메뉴 dialog 표시
    
    TEST
    ----
    - 사용자 메뉴 버튼 클릭 시 dialog가 표시되는지 확인
    
    NOTE
    ----
    - 실제 버튼 및 dialog의 layout은 변경할 예정

---
+++ packages/frontend/src/components/Header/User/index.tsx
@@ -0,0 +1,45 @@
+import { useRef } from 'react';
+import { useRecoilValue } from 'recoil';
+import { shouldRefreshAtom } from '~/recoil/atoms';
+
+const HeaderUser = () => {
+  const parentRef = useRef<HTMLSpanElement>(null);
+  const dialogRef = useRef<HTMLDialogElement>(null);
+  const refreshed = useRecoilValue(shouldRefreshAtom);
+
+  const getElement = () => {
+    const wrapper = parentRef.current;
+    const dialog = dialogRef.current;
+
+    return { dialog, wrapper };
+  };
+
+  const onMenuClick = () => {
+    const { dialog } = getElement();
+    if (!dialog) return;
+
+    dialog.open ? dialog.close() : dialog.show();
+  };
+
+  globalThis.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const { dialog, wrapper } = getElement();
+    if (!dialog || !wrapper) return;
+
+    if (!wrapper.contains(e.target as Node)) dialog.close();
+  });
+
+  return (
+    <>
+      <span className="header__user" ref={parentRef} onClick={onMenuClick}>
+        user menu
+      </span>
+      <dialog className="header__user--dialog" ref={dialogRef}>
+        {refreshed ? 'sign out' : 'sign in'}
+      </dialog>
+    </>
+  );
+};
+
+export default HeaderUser;
