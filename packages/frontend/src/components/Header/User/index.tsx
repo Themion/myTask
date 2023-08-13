0695a8a44dbd729bd5bda2b19e97b0817353e15a
Themion <themion@naver.com>
2023-08-14 00:52:09 +0900

    feat : 로그인 되어있지 않은 경우 로그인 modal 표시
    DESC
    ----
    - 로그인 되어있지 않은 경우 로그인 modal 표시
    
    NOTE
    ----
    - 로그인 되어있는 경우 아무 동작 하지 않음

--- packages/frontend/src/components/Header/User/index.tsx
+++ packages/frontend/src/components/Header/User/index.tsx
@@ -1,10 +1,21 @@
+import { MouseEventHandler, useRef } from 'react';
 import { useRecoilValue } from 'recoil';
 import Dropdown from '~/components/Dropdown';
+import Modal from '~/components/Modal';
 import { shouldRefreshAtom } from '~/recoil/atoms';
+import AuthRequest from '~/routes/Auth/request';
 import styles from './styles.module.scss';
 
 const HeaderUser = () => {
   const refreshed = useRecoilValue(shouldRefreshAtom);
+  const modalRef = useRef<HTMLDialogElement>(null);
+
+  const onClick: MouseEventHandler = () => {
+    const modal = modalRef.current;
+    if (!modal) return;
+
+    modal.showModal();
+  };
 
   return (
     <Dropdown
@@ -12,7 +23,16 @@ const HeaderUser = () => {
       classList={[styles.user]}
       dropdownClassList={[styles.dropdown]}
     >
-      {refreshed ? 'sign out' : 'sign in'}
+      {refreshed ? (
+        <span>sign out</span>
+      ) : (
+        <>
+          <span onClick={onClick}>sign in</span>
+          <Modal title="Test Title" ref={modalRef}>
+            <AuthRequest />
+          </Modal>
+        </>
+      )}
     </Dropdown>
   );
 };
