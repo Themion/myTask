144116a7bcce60e43c4c80d7039d54746442f8ea
Themion <themion@naver.com>
2023-08-13 19:45:09 +0900

    feat : dropdown 컴포넌트 생성
    DESC
    ----
    - dropdown 버튼과 content를 포함하는 컴포넌트 생성

--- packages/frontend/src/components/Header/User/index.tsx
+++ packages/frontend/src/components/Header/User/index.tsx
@@ -1,43 +1,19 @@
-import { useRef } from 'react';
 import { useRecoilValue } from 'recoil';
+import Dropdown from '~/components/Dropdown';
 import { shouldRefreshAtom } from '~/recoil/atoms';
 import styles from './styles.module.scss';
 
 const HeaderUser = () => {
-  const parentRef = useRef<HTMLDivElement>(null);
-  const dropdownRef = useRef<HTMLDivElement>(null);
   const refreshed = useRecoilValue(shouldRefreshAtom);
 
-  const getElement = () => {
-    const wrapper = parentRef.current;
-    const dropdown = dropdownRef.current;
-
-    return { dropdown, wrapper };
-  };
-
-  const onMenuClick = () => {
-    const { dropdown } = getElement();
-    if (!dropdown) return;
-
-    dropdown.classList.toggle(styles.hidden);
-  };
-
-  globalThis.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    const { dropdown, wrapper } = getElement();
-    if (!wrapper || !dropdown) return;
-
-    if (!wrapper.contains(e.target as Node)) dropdown.classList.add(styles.hidden);
-  });
-
   return (
-    <div className={styles.user} ref={parentRef}>
-      <span onClick={onMenuClick}>user menu</span>
-      <div className={`${styles.dropdown} ${styles.hidden}`} ref={dropdownRef}>
-        {refreshed ? 'sign out' : 'sign in'}
-      </div>
-    </div>
+    <Dropdown
+      button={<span>user menu</span>}
+      classList={[styles.user]}
+      dropdownClassList={[styles.dropdown]}
+    >
+      {refreshed ? 'sign out' : 'sign in'}
+    </Dropdown>
   );
 };
 
