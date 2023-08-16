b474192ec1f6a980963e699f32804536e0af7c87
Themion <themion@naver.com>
2023-08-17 00:25:07 +0900

    feat : group list에 paging 기능 생성
    DESC
    ----
    - group 목록 조회 시 page 값을 이용해 조회 범위 조정

--- packages/frontend/src/mock/handlers/group.ts
+++ packages/frontend/src/mock/handlers/group.ts
@@ -5,14 +5,17 @@ import mockDir from './mockDir';
 const mockGroupDir = (path: string) => mockDir(`/group${path}`);
 
 const groupHandlers = [
-  rest.get(mockGroupDir(''), async (_, res, ctx) => {
+  rest.get(mockGroupDir(''), async (req, res, ctx) => {
     ctx.delay();
 
-    const group: GroupListDTO['group'] = [
-      { id: 1, name: 'name1' },
-      { id: 2, name: 'name2' },
-      { id: 3, name: 'name3' },
-    ];
+    const params = req.url.searchParams;
+    const page = parseInt(params.get('page') ?? '1');
+    const limit = parseInt(params.get('limit') ?? '10');
+
+    const group: GroupListDTO['group'] = new Array(13)
+      .fill(0)
+      .map((_, i) => ({ id: i, name: `name${i}` }))
+      .slice((page - 1) * limit, page * limit);
 
     return res(ctx.status(200), ctx.json({ group }));
   }),
