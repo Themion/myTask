f05ece1a50bd76ec874b054e27aacb9207284bc2
Themion <themion@naver.com>
2023-08-19 00:14:29 +0900

    feat : group 추가 버튼을 컴포넌트로 생성
    DESC
    ----
    - 사용자가 member로 존재하는 새로운 그룹을 생성하는 버튼을 생성

---
+++ packages/frontend/src/components/Group/Add/index.tsx
@@ -0,0 +1,60 @@
+import { FormEventHandler, MouseEventHandler, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import createGroup from '~/api/group/create';
+import { Modal } from '~/components';
+import styles from './styles.module.scss';
+
+const GroupAdd = () => {
+  const modalRef = useRef<HTMLDialogElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
+  const mutation = createGroup({
+    onSuccess: () => navigate(0),
+  });
+
+  const getElements = () => {
+    const modal = modalRef.current;
+    const input = inputRef.current;
+
+    return { modal, input };
+  };
+
+  const onClick: MouseEventHandler = () => {
+    const { modal } = getElements();
+    if (!modal) return;
+
+    modal.showModal();
+  };
+
+  const onSubmit: FormEventHandler = (e) => {
+    e.preventDefault();
+
+    const { modal, input } = getElements();
+    if (!input) return;
+
+    const name = input.value;
+    mutation.mutate({ name });
+    if (modal) modal.close();
+  };
+
+  const isDisabled = mutation.isLoading;
+
+  return (
+    <>
+      <button className={styles.add} onClick={onClick}>
+        <span className={styles.text}>+</span>
+      </button>
+      <Modal title="New Group" ref={modalRef}>
+        <form onSubmit={onSubmit}>
+          <label htmlFor="group-name">Group Name: </label>
+          <input id="group-name" type="text" ref={inputRef} />
+          <button type="submit" disabled={isDisabled} aria-disabled={isDisabled}>
+            create
+          </button>
+        </form>
+      </Modal>
+    </>
+  );
+};
+
+export default GroupAdd;
---
+++ packages/frontend/src/components/Group/index.ts
@@ -0,0 +1,2 @@
+import GroupAdd from './Add';
+export { GroupAdd };
