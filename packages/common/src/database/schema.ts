14bff42ed1b7efc57983215bd70a635fcf56f394
Themion <themion@naver.com>
2023-08-20 02:36:09 +0900

    feat : member 테이블에 constraint 추가
    DESC
    ----
    - 한 그룹에 같은 사용자가 여러 번 생기는 것을 방지해야 함
    - group id와 user id를 하나로 묶어 constraint 생성

--- packages/common/src/database/schema.ts
+++ packages/common/src/database/schema.ts
@@ -1,4 +1,4 @@
-import { boolean, integer, pgTable, serial, varchar } from 'drizzle-orm/pg-core';
+import { boolean, integer, pgTable, serial, unique, varchar } from 'drizzle-orm/pg-core';
 
 const users = pgTable('users', {
   id: serial('id').primaryKey(),
@@ -10,16 +10,22 @@ const groups = pgTable('groups', {
   name: varchar('name', { length: 20 }).notNull(),
 });
 
-const members = pgTable('users_to_groups', {
-  id: serial('id').primaryKey(),
-  groupId: integer('group_id')
-    .notNull()
-    .references(() => groups.id),
-  userId: integer('user_id')
-    .notNull()
-    .references(() => users.id),
-  name: varchar('name', { length: 20 }).notNull().default('member'),
-  isManager: boolean('is_manager').default(false),
-});
+const members = pgTable(
+  'users_to_groups',
+  {
+    id: serial('id').primaryKey(),
+    groupId: integer('group_id')
+      .notNull()
+      .references(() => groups.id),
+    userId: integer('user_id')
+      .notNull()
+      .references(() => users.id),
+    name: varchar('name', { length: 20 }).notNull().default('member'),
+    isManager: boolean('is_manager').default(false),
+  },
+  (table) => ({
+    unique: unique().on(table.groupId, table.userId),
+  }),
+);
 
 export { users, groups, members };
