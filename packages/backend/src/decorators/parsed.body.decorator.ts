096666ad605f13293ae255295f868e3ed7cb2e5e
Themion <themion@naver.com>
2023-09-13 00:03:40 +0900

    feat : ParsedBody 데코레이터가 제대로 동작하도록 수정
    DESC
    ----
    - ParsedBody 데코레이터의 정의 부분에서 createParamDecorator 메소드를 사용하지 않고 Body 데코레이터를 반환하도록 변경

--- packages/backend/src/decorators/parsed.body.decorator.ts
+++ packages/backend/src/decorators/parsed.body.decorator.ts
@@ -1,9 +1,7 @@
-import { Body, createParamDecorator } from '@nestjs/common';
+import { Body } from '@nestjs/common';
 import { ZodSchema } from 'zod';
 import { ZodParsePipe } from '~/pipe';
 
-const ParsedBody = createParamDecorator(<T extends ZodSchema>(schema: T) =>
-  Body(new ZodParsePipe(schema)),
-);
+const ParsedBody = <T extends ZodSchema>(schema: T) => Body(new ZodParsePipe(schema));
 
 export default ParsedBody;
